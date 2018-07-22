82dff232f061841d4cccb0235f7a10e549c3b932
jimmy hu <5549662+Jimmy-Hu@users.noreply.github.com>
2018-07-22 11:25:07 -0400

    Update main.ino
    Update main.ino

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -5,3 +5,14 @@
  *  Arduino Digital Pin4連接至繼電器，輸出High時SSR OFF，輸出Low時SSR ON。
  *  Version 2 : 藍芽傳'Lock_open'門鎖開，藍芽傳'Lock_close'門鎖關。
  */
+
+
+//***函式庫引用區***
+#include  <SoftwareSerial.h>                                                            //  引用<SoftwareSerial.h>函式庫
+//***定義變數區***
+#define Lock_Control_Pin 4                                                              //  定義電鎖控制接腳為第4接腳
+#define Bluetooth_KEY 5                                                                 //  定義藍芽傳輸模組KEY接腳連接至arduino第5接腳
+#define Bluetooth_RxD 8                                                                 //  定義藍芽傳輸模組RxD接腳連接至arduino第8接腳
+#define Bluetooth_TxD 7                                                                 //  定義藍芽傳輸模組TxD接腳連接至arduino第7接腳
+#define History_quota 20                                                                //  Define History_quota - a quota of storage of history data
+
