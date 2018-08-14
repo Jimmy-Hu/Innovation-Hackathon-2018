342ecda9f0c9941ccd27e571002998c97d221a69
jimmy hu <5549662+Jimmy-Hu@users.noreply.github.com>
2018-08-14 00:08:58 -0400

    Update ADXL345Test.ino
    Update ADXL345Test.ino

--- Arduino/ADXL345/ADXL345Test.ino
+++ Arduino/ADXL345/ADXL345Test.ino
@@ -107,3 +107,33 @@ void displayRange(void)
   Serial.println(" g");  
 }
 
+oid setup(void) 
+{
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+  Serial.begin(9600);
+  Serial.println("Accelerometer Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+  // displaySetRange(ADXL345_RANGE_8_G);
+  // displaySetRange(ADXL345_RANGE_4_G);
+  // displaySetRange(ADXL345_RANGE_2_G);
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Display additional settings (outside the scope of sensor_t) */
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+}
