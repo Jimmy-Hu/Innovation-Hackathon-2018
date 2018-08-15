1aea76c538c570459f7e0c86a9593d3ccebce3c4
jimmy hu <5549662+Jimmy-Hu@users.noreply.github.com>
2018-08-15 18:28:42 -0400

    Update ADXL345Test.ino
    Update ADXL345Test.ino

--- Arduino/ADXL345/ADXL345Test.ino
+++ Arduino/ADXL345/ADXL345Test.ino
@@ -137,3 +137,16 @@ oid setup(void)
   displayRange();
   Serial.println("");
 }
+
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  delay(500);
+}
